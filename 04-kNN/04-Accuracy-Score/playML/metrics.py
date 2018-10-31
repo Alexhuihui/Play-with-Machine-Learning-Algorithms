b60855f1cc62e707047b6e176e2d08cad61f7162
wangyonghui <2930807240@qq.com>
2018-10-31 13:34:46 +0800

    学习

--- 04-kNN/04-Accuracy-Score/playML/metrics.py
+++ 04-kNN/04-Accuracy-Score/playML/metrics.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def accuracy_score(y_true, y_predict):
+def accuracy_score(y_predict, y_true):
     '''计算y_true和y_predict之间的准确率'''
     assert y_true.shape[0] == y_predict.shape[0], \
         "the size of y_true must be equal to the size of y_predict"
